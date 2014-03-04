cce80020b19b55712abd74d01bbbca0c2aca6cd7
leafc <coohcooh@126.com>
2014-03-04 09:49:23 +0800

    first commit

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by cameron on 14-3-3.
+//  Copyright (c) 2014年 Cameron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by cameron on 14-3-3.
+//  Copyright (c) 2014年 Cameron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
